bbd3a3a918040a75018d77216e93626c85a4913f
ADMIN <ADMIN@DESKTOP-63TRQ0M>
2023-12-08 15:57:52 +0530

    8 DEC 2023

---
+++ _01_JAVA_AND_SELENIUM/src/_J_01_Simple_Programs/_08_Add_.java
@@ -0,0 +1,13 @@
+package _J_01_Simple_Programs;
+
+public class _08_Add_ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 10;
+int b = 20;
+int c=a+b;
+System.out.println("Total = "+c);
+	}
+
+}
