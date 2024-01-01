eb21a14de81b6993052a8aa5a46f76e6a2e8117d
SASIHAR <SASIHAR@SASIHAR-PC>
2024-01-01 22:29:11 +0530

    01 Jan 2024

--- _01_JAVA_AND_SELENIUM/src/_S_05_02_Browser/Open_Browser_01_Sasi_Basic_2020.java
+++ _01_JAVA_AND_SELENIUM/src/_S_05_02_Browser/Open_Browser_01_Sasi_Basic_2020.java
@@ -11,7 +11,7 @@ public class Open_Browser_01_Sasi_Basic_2020 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.setProperty("webdriver.chrome.driver", "C:\\SELENIUM JARS\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver\\chromedriver.exe");
 		WebDriver d = new ChromeDriver();
 		d.get("https:\\www.google.co.in");
 		
