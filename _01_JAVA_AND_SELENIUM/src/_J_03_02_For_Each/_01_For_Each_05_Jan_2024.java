aabcddf11c9de46476041931e901bac560158df6
ADMIN <ADMIN@DESKTOP-63TRQ0M>
2024-01-10 16:07:33 +0530

    10 Jan 2024

--- _01_JAVA_AND_SELENIUM/src/_J_03_02_For_Each/_01_For_Each_05_Jan_2024.java
+++ _01_JAVA_AND_SELENIUM/src/_J_03_02_For_Each/_01_For_Each_05_Jan_2024.java
@@ -4,6 +4,13 @@ public class _01_For_Each_05_Jan_2024 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// FOR EACH IS USED TO CHECK THE EACH VALUE STORED AND DO THE WORK
+		// FOR LOOP IS USED TO LOOP TILL THE CONDITION FULFILL
+		
+		// FOR EACH CHECK THE CONDITION AND ENTER THE BRACKET{} AND DO THE WORK
+	
+		
 char vowels[] = {'a', 'e','i','o', 'U'};
 
 for (char c : vowels) {
--- _01_JAVA_AND_SELENIUM/src/_S_05_11_02_Windows_Handle/Window_Handle_06_Sasi_4_Jan_24.java
+++ _01_JAVA_AND_SELENIUM/src/_S_05_11_02_Windows_Handle/Window_Handle_06_Sasi_4_Jan_24.java
@@ -16,19 +16,47 @@ public class Window_Handle_06_Sasi_4_Jan_24 {
 		d.manage().window().maximize();
 		String First_Window = d.getWindowHandle();
 		d.navigate().to("https://www.leafground.com/window.xhtml");
-		
+
 		WebElement Open_Box = d.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/form/div/div[1]/div[1]/button/span"));
 		Open_Box.click();
-		
+
 		Set<String> All_Windows = d.getWindowHandles();
-		
+
 		for (String Second_Window : All_Windows) {
 			d.switchTo().window(Second_Window);
-			
 		}
-Thread.sleep(2000);
-d.close();
+		Thread.sleep(2000);
+		d.close();
 
+		d.switchTo().window(First_Window);
+
+		WebElement Open_Multiple = d.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/form/div/div[1]/div[2]/button/span"));
+		Open_Multiple.click();
+
+		Set<String> Open_Multiple2 = d.getWindowHandles();
+		int Open_Multiple_size = Open_Multiple2.size();
+		System.out.println("Open_Multiple_size = "+Open_Multiple_size);
+		d.switchTo().window(First_Window);
+
+		WebElement Close_Window = d.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/form/div/div[2]/div[1]/button/span[2]"));
+		Close_Window.click();
+
+		Set<String> Close_All_Window = d.getWindowHandles();
+		int Close_All_Window_size = Close_All_Window.size();
+		System.out.println("After Clicking Close Windows = "+Close_All_Window_size);
+	
+		
+		for (String All_Opened_windows : Close_All_Window) {
+			if (!All_Opened_windows.equals(First_Window)) {
+			d.switchTo().window(All_Opened_windows);
+				Thread.sleep(2000);
+				d.close();
+			}
+		
+			Thread.sleep(2000);
+			d.quit();
+		}
 	}
 
 }
+
