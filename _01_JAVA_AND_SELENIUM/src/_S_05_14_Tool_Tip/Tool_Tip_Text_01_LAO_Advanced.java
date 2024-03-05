4f64b3b806bbb5f36cb8f0d4fdfdae4dfa559c9d
ADMIN <ADMIN@DESKTOP-63TRQ0M>
2024-03-05 16:12:14 +0530

    05 MAR 2024

--- _01_JAVA_AND_SELENIUM/src/_S_05_14_Tool_Tip/Tool_Tip_Text_01_LAO_Advanced.java
+++ _01_JAVA_AND_SELENIUM/src/_S_05_14_Tool_Tip/Tool_Tip_Text_01_LAO_Advanced.java
@@ -5,11 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Tool_Tip_Text_LAO_Advanced {
+public class Tool_Tip_Text_01_LAO_Advanced {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.gecko.driver", "C:\\SELENIUM JARS\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", "C:\\WebDriver\\geckodriver\\geckodriver.exe");
 		WebDriver d=new FirefoxDriver();
 		d.get("http://www.leafground.com/pages/tooltip.html");
 		WebElement name = d.findElement(By.id("age"));
