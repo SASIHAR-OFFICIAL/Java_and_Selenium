aa827a8dd6e2f5854987eaf9a9feef7c212e0750
SASIHAR <SASIHAR@SASIHAR-PC>
2024-06-30 23:11:35 +0530

    30 JUNE 24 FROM HOME

--- _01_JAVA_AND_SELENIUM/src/_S_05_11_02_Windows_Handle/_15_Window_Handle_Sasi_25_Jun_24.java
+++ _01_JAVA_AND_SELENIUM/src/_S_05_11_02_Windows_Handle/_15_Window_Handle_Sasi_25_Jun_24.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class _15_Window_Handle_Sasi_25_Jun_24 {
@@ -19,24 +20,26 @@ public class _15_Window_Handle_Sasi_25_Jun_24 {
 		WebElement Home_page = d.findElement(By.xpath("//*[@id=\"home\"]"));
 		Home_page.click();
 		Thread.sleep(1000);
+		//d.switchTo().newWindow(WindowType.TAB);
 		
-		Set<String> windowHandles_2 = d.getWindowHandles();
-		for (String windowHandles_1_2 : windowHandles_2) {
-			d.switchTo().window(windowHandles_1_2);
-			String windowHandles_2_title = d.getTitle();
-			System.out.println("window_1_title = "+windowHandles_2_title);
-			Thread.sleep(1000);
-			d.close();
-		}
+		String window_2_title = d.getTitle();
+		System.out.println("window_2_title = "+window_2_title);
 		
-//
+//		Set<String> windowHandles_2 = d.getWindowHandles();
+//		String windowHandles_2_title = d.getTitle();
+//		
+//		System.out.println("window_2_title = "+windowHandles_2_title);
+//		Thread.sleep(1000);
+//		
+//		for (String windowHandles_1_2 : windowHandles_2) {
+//			d.switchTo().window(windowHandles_1_2);
+//			if (windowHandles_1_2.equals(d.getCurrentUrl().equals("https://letcode.in/test"))) {
+//				d.close();
+//			}
+//			//d.switchTo().window(windowHandle_1);
+//			}
+		
 //		d.switchTo().window(windowHandle_1);
 //		WebElement Muiltiple_windows = d.findElement(By.xpath("//*[@id=\"multi\"]"));
 //		Muiltiple_windows.click();
-		
-
-		//		
-		//		
-
-
-	}}
+		}}
