4ee5b76dd6661204475e8b931b3010fda0049cdb
ADMIN <ADMIN@DESKTOP-63TRQ0M>
2024-07-18 16:10:57 +0530

    18 JUL 2024 FROM OFFICE

---
+++ _01_JAVA_AND_SELENIUM/src/_J_02_03_Function/_01_Function_Example3.java
@@ -0,0 +1,22 @@
+package _J_02_03_Function;
+
+public class _01_Function_Example3 {
+
+	void Greeting()
+	{
+		System.out.println("Vanakam");
+		byebye();
+	}
+	
+	void byebye() {
+		System.out.println("Bye Bye");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		_01_Function_Example3 obj = new _01_Function_Example3();
+		obj.Greeting();
+	
+	}
+
+}
